dfab164b5b4b63e8c9a200ecf2bafee66ae135d4
Andre Sousa lajas <asousa-l@c1r15s6.42lisboa.com>
2022-01-14 13:36:48 +0000

    Shell01

--- ex08/add_chelou.sh
+++ ex08/add_chelou.sh
@@ -1,4 +1,2 @@
-FT_NBR1=\"\"!\"\"!\"\"!\"\"!\"\"!\"\"
-FT_NBR2=dcrcmcmooododmrrrmorcmcrmomo
-gtaioluSnemf = FT_NBR1 + FT_NBR2
-echo $gtaioluSnemf
+#!/bin/sh
+echo $FT_NBR1 + $FT_NBR2 | sed "s/\'/0/g" | tr '\\\"\?\!' 1234 | tr 'mrdoc' 01234 | xargs echo 'obase=13; ibase=5;' | bc | tr 0123456789ABC 'gtaio luSnemf'
